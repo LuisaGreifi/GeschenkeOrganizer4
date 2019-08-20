938daf34b259d5d87c57eb95174232df25309b31
obermaierveronika <42035834+obermaierveronika@users.noreply.github.com>
2019-08-20 21:59:45 +0200

    Beginn Queries

--- app/src/main/java/com/example/geschenkeorganizer/database/DaoAccess.java
+++ app/src/main/java/com/example/geschenkeorganizer/database/DaoAccess.java
@@ -1,11 +1,19 @@
 package com.example.geschenkeorganizer.database;
 
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 public interface DaoAccess {
     // Person Access
      @Insert
      void insertPerson(Person person);
+
+     //aus der Übung 6
+     @Query("SELECT * FROM Person")
+    List<Person> getAll();
+
     //Event Access
     @Insert
      void insertEvent(Event event);
