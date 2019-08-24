a68261427cc72cd544579e23737f204e95a37ba2
obermaierveronika <42035834+obermaierveronika@users.noreply.github.com>
2019-08-24 15:16:31 +0200

    DB Klasse aktualisiert (nur 1 Instanz gleichzeitig möglich)

--- app/src/main/java/com/example/geschenkeorganizer/database/MyDatabase.java
+++ app/src/main/java/com/example/geschenkeorganizer/database/MyDatabase.java
@@ -1,9 +1,31 @@
 package com.example.geschenkeorganizer.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities={Person.class, Event.class, PersonEventJoin.class, Present.class}, version = 1, exportSchema=false)
 public abstract class MyDatabase extends RoomDatabase {
         public abstract DaoAccess daoAccess();
+
+        /** https://codelabs.developers.google.com/codelabs/android-training-livedata-viewmodel/#6
+         * --> es kann nicht mehrere Datenbank-Instanzen gleichzeitig geben
+         */
+        private static MyDatabase INSTANCE;
+
+        public static MyDatabase getDatabase(final Context context) {
+                if (INSTANCE == null) {
+                        synchronized (MyDatabase.class) {
+                                if (INSTANCE == null) {
+                                        INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                                MyDatabase.class, "my_database")
+                                                .fallbackToDestructiveMigration()
+                                                .build();
+                                }
+                        }
+                }
+                return INSTANCE;
+        }
 }
