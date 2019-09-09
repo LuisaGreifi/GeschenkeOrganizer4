20a069dfb64ae62770258cc2d69c3932e0dde2fb
obermaierveronika <42035834+obermaierveronika@users.noreply.github.com>
2019-09-09 22:14:54 +0200

    Alles sichtbar Problem: Einträge mehrfach angezeigt todo: presentListAddFragment

--- app/src/main/java/com/example/geschenkeorganizer/presents/PresentsActivity.java
+++ app/src/main/java/com/example/geschenkeorganizer/presents/PresentsActivity.java
@@ -2,11 +2,14 @@ package com.example.geschenkeorganizer.presents;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.geschenkeorganizer.R;
 
