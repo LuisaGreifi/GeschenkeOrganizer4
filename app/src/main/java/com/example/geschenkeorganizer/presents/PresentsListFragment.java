b330e449b67bd511ddb56977bc3ce193d7a4236a
obermaierveronika <42035834+obermaierveronika@users.noreply.github.com>
2019-09-09 21:05:50 +0200

    schaut soweit gut aus, aber funktioniert nicht todo: presentListAddFragment

--- app/src/main/java/com/example/geschenkeorganizer/presents/PresentsListFragment.java
+++ app/src/main/java/com/example/geschenkeorganizer/presents/PresentsListFragment.java
@@ -1,25 +1,72 @@
 package com.example.geschenkeorganizer.presents;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.geschenkeorganizer.R;
+import com.example.geschenkeorganizer.database.PresentListAdapter;
+import com.example.geschenkeorganizer.database.PresentRepresentation;
+import com.example.geschenkeorganizer.database.PresentViewModel;
 
+import java.util.List;
+
+//todo: NEU
+// https://medium.com/@tonia.tkachuk/android-app-example-using-room-database-63f7091e69af
+// eigtl ganze Klasse, Platzierung Methoden an richtiger Stelle + Kontext!
 
 
 public class PresentsListFragment extends Fragment {
+
+    private PresentListAdapter presentListAdapter;
+    private PresentViewModel presentViewModel;
+    private Context context;
 
     public PresentsListFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_presents_list, container, false);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        presentListAdapter = new PresentListAdapter(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //todo: neu
+        // https://codelabs.developers.google.com/codelabs/android-room-with-a-view/#13
+        presentViewModel = ViewModelProviders.of(this).get(PresentViewModel.class);
+        presentViewModel.getAllPresents().observe(this, new Observer<List<PresentRepresentation>>() {
+            @Override
+            public void onChanged(@Nullable final List<PresentRepresentation> presents) {
+                // Update the cached copy of the words in the adapter.
+                presentListAdapter.setPresents(presents);
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_presents_list, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setAdapter(presentListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/example/geschenkeorganizer/presents/PresentsListFragmentOld.java
@@ -0,0 +1,111 @@
+package com.example.geschenkeorganizer.presents;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.geschenkeorganizer.R;
+import com.example.geschenkeorganizer.presents.Present;
+
+// Code von NoteTaker-Übung
+
+public class PresentsListFragmentOld extends ListFragment {
+    //todo: NEU (ausgeklammert: Interfaces, nicht benötigt zwecks LiveData)
+    /**
+     private OnListItemSelectedListener mCallback;
+
+
+     public interface OnListItemSelectedListener {
+     public void onListItemSelected(int id);
+     }
+     */
+
+    public PresentsListFragmentOld() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_presents_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //todo: NEU (ausgeklammert: läuft über PresentListAdapter usw.)
+
+        // populateList();
+    }
+
+    //todo: NEU (ausgeklammert: läuft über PresentListAdapter usw.)
+    /**
+     protected void populateList() {
+     //todo: mit Datenbank verbinden (für Array)
+
+     Present[] values = new Present[0];
+     if (values.length == 0) {
+     Toast.makeText(getActivity(), R.string.text_noPresents,
+     Toast.LENGTH_LONG).show();
+     }
+     else {
+     ArrayAdapter<Present> adapter =
+     new ArrayAdapter<Present>(getActivity(), android.R.layout.simple_list_item_1,
+     values);
+     setListAdapter(adapter);
+     }
+     }
+     */
+
+    //todo: NEU (ausgeklammert: läuft über PresentListAdapter usw.)
+    /**
+     @Override
+     public void onResume() {
+     super.onResume();
+     populateList();
+     }
+     */
+
+    //todo: NEU (ausgeklammert: später den Fall behandeln!)
+    /**
+     @Override
+     public void onListItemClick(ListView l, View v, int position, long id) {
+     Present clickedPresent = (Present) l.getItemAtPosition(position);
+     //todo: ??? evtl. Geschenk verändern lassen -> GeschenkeHinzufügen-Fragment einfügen
+     Toast.makeText(getActivity(),
+     "Clicked on pos: " + position + "; id: " + id,
+     Toast.LENGTH_LONG).show();
+     // Prevent opening the dummy entry
+     if (clickedPresent.getId() == -1) {
+     return;
+     }
+     //todo: NEU (Callback erstmal ausgeklammert)
+     // mCallback.onListItemSelected(clickedPresent.getId());
+     }
+     */
+
+    //todo: NEU (ausgeklammert: Interface/Callback)
+    /**
+     @SuppressWarnings("deprecation")
+     // The new method onAttach(Context context) doesn't exist in API level 22 and below
+     @Override
+     public void onAttach(Activity activity) {
+     super.onAttach(activity);
+     // This makes sure that the container activity has implemented
+     // the callback interface. If not, it throws an exception
+     try {
+     mCallback = (OnListItemSelectedListener) activity;
+     } catch (ClassCastException e) {
+     throw new ClassCastException(
+     activity.toString() + " must implement OnListItemSelectedListener!");
+     }
+     }
+     */
+
+}
